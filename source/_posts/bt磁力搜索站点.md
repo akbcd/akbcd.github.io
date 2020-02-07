a40ea7702b8a348fcd8b77a1c7ae6d151f2b8ea3
“akbcd” <2226227487@qq.com>
2020-02-07 21:32:14 +0800

    ...

--- source/_posts/bt磁力搜索站点.md
+++ source/_posts/bt磁力搜索站点.md
@@ -1,21 +1,27 @@
 ---
 title: bt磁力搜索站点
-date: 2019-11-21 22:34:16
+date: 2020-02-07 21:05:00
 tags: bt磁力
 ---
 推荐几个bt磁力搜索站点
 <!--more-->
 ## BT1207
 资源多，更新快的磁力链接搜索引擎
-[https://bt1207.monster/](https://bt1207.monster/)
+[https://bt1207cli.buzz/](https://bt1207cli.buzz/)
 ## SkrBT
 索引了上千万的磁力链接，全银河系资源最丰富的种子搜索网站
-[https://skrbt.monster/](https://skrbt.monster/)
-## RARBG
-老牌知名BT磁力搜索引擎
-[https://rarbgaccess.org/index70.php](https://rarbgaccess.org/index70.php)
+[https://skrbt15.icu/](https://skrbt15.icu/)
+## 老王磁力
+索引了全球最新最热门的BT种子信息和磁力链接
+[https://laowangcili.monster/](https://laowangcili.monster/)
+## AIO Search
+知名的磁力聚合搜索网站，几乎聚合了所有你能想得到的磁力搜索站！
+[https://www.aiosearch.com/](https://www.aiosearch.com/)
 ## 东京图书馆
 知名站点
 [https://www.tokyotosho.info/](https://www.tokyotosho.info/)
-## 最后
+
 以上内容均来自[https://no404.icu/](https://no404.icu/)
+## 磁力宝
+最佳的磁力搜索引擎
+[http://www.clb.biz/](http://www.clb.biz/)
--- themes/yilia/source/js/script.js
+++ themes/yilia/source/js/script.js
@@ -1,27 +1,4 @@
-// progress bar init
-const progressElement = window.document.querySelector('.progress-bar');
-if (progressElement) {
-    new ScrollProgress((x, y) => {
-        progressElement.style.width = y * 100 + '%';
-    });
-}
-// 代码块复制
-function copy(button){
-    new ClipboardJS('#js-btn-copy', {
-        target: function(trigger) {
-            if(trigger.nextElementSibling != null){
-                $(button).html("复制成功");
-                setTimeout(function(){ $(button).html("复制") }, 1000);
-                return trigger.nextElementSibling;
-            }
-        }
-    });
-}
-/*页面载入完成后，创建复制按钮*/
-!function (e, t, a) {
-    var initCopyCode = function(){
-        var copyHtml = '<button id="js-btn-copy" onclick="copy(this)" >复制</button>';
-        $(".code pre").before(copyHtml);
-    }
-    initCopyCode();
-}(window, document);
+const progressElement=window.document.querySelector('.progress-bar');if(progressElement){new ScrollProgress((x,y)=>{progressElement.style.width=y*100+'%';});}
+function copy(button){new ClipboardJS('#js-btn-copy',{target:function(trigger){if(trigger.nextElementSibling!=null){$(button).html("复制成功");setTimeout(function(){$(button).html("复制")},1000);return trigger.nextElementSibling;}}});}
+!function(e,t,a){var initCopyCode=function(){var copyHtml='<button id="js-btn-copy" onclick="copy(this)" >复制</button>';$(".code pre").before(copyHtml);}
+initCopyCode();}(window,document);
