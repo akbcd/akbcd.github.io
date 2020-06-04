121c3e8bad3e8d06a2bc8376b2676055fd535e97
“akbcd” <2226227487@qq.com>
2020-06-04 14:10:17 +0800

    ...

--- themes/yilia/source/backup/backup_script.js
+++ themes/yilia/source/backup/backup_script.js
@@ -1,4 +1,4 @@
-/* 
+/*
 progress bar init
 mobile底部进度条
 */
@@ -8,7 +8,8 @@ if (progressElement) {
         progressElement.style.width = y * 100 + '%';
     });
 }
-/* 
+
+/*
 toc目录优化，jq实现锚点跳转
 */
 // 隐藏子目录
@@ -18,6 +19,25 @@ $('.toc-link').on('click', function (e) {
     e.preventDefault()
     toToc($(this).attr('href'))
 })
+function toToc (name) {
+  //pc锚点跳转
+  var scrollPositionTop = $(name).position().top+50
+  $('#container').animate({
+    scrollTop: scrollPositionTop
+  },1000)
+  //mobile锚点跳转
+  var scrollOffset = $(name).offset()
+  $('body,html').animate({
+    scrollTop: scrollOffset.top-50
+  })
+};
+// 更新锚点url
+var isanchor = yiliaConfig.anchor
+var updateAnchor = function (anchor) {
+  if (window.history.replaceState && anchor !== window.location.hash) {
+    window.history.replaceState(undefined, undefined, anchor)
+  }
+}
 // 显示子目录
 var expandToc = function ($item) {
   if ($item.is(':visible')) {
@@ -25,24 +45,24 @@ var expandToc = function ($item) {
   }
   $item.fadeIn(400)
 }
-// 锚点url
-var isanchor = yiliaConfig.anchor
-var updateAnchor = function (anchor) {
-  if (window.history.replaceState && anchor !== window.location.hash) {
-    window.history.replaceState(undefined, undefined, anchor)
+// 目录滚动
+var autoScrollToc = function () {
+  if ($('.toc-link').hasClass('active')) {
+    var activePosition = $('.active').position().top
+    var articleScrolltop = $('.tooltip-content .toc-article').scrollTop()
+    if (activePosition > $('.tooltip-content .toc-article').height()-50) {
+      $('.tooltip-content .toc-article').scrollTop(articleScrolltop + 100)
+    }
+    if (activePosition < 50) {
+      $('.tooltip-content .toc-article').scrollTop(articleScrolltop - 100)
+    }
   }
 }
-//pc目录优化
-//pc滚动监听，实时获取滚动高度
+// pc动态目录
 $('#container').scroll(function() {
   var currentTop = $(this).scrollTop()
   findPcHeadPosition(currentTop)
-})
-//mobile目录优化
-//mobile滚动监听，实时获取滚动高度
-$(window).scroll(function() {
-  var currentTop = $(this).scrollTop()||document.documentElement.scrollTop
-  findMobileHeadPosition(currentTop+50)
+  autoScrollToc()
 })
 // find head position & add active class
 // DOM Hierarchy:
@@ -91,6 +111,12 @@ var findPcHeadPosition = function (top) {
     .find('.toc-child').hide()
   }
 }
+// mobile动态目录
+$(window).scroll(function() {
+  var currentTop = $(this).scrollTop()||document.documentElement.scrollTop
+  findMobileHeadPosition(currentTop+50)
+  autoScrollToc()
+})
 var findMobileHeadPosition = function (top) {
 // assume that we are not in the post page if no TOC link be found,
 // thus no need to update the status
@@ -134,15 +160,3 @@ var findMobileHeadPosition = function (top) {
     .find('.toc-child').hide()
   }
 }
-function toToc (name) {
-  //pc
-  var scrollPositionTop = $(name).position().top+50
-  $('#container').animate({
-    scrollTop: scrollPositionTop
-  },1000)
-  //mobile
-  var scrollOffset = $(name).offset()
-  $('body,html').animate({
-    scrollTop: scrollOffset.top-50
-  })
-};
--- themes/yilia/source/js/script.js
+++ themes/yilia/source/js/script.js
@@ -1,23 +1,27 @@
 "use strict";var progressElement=window.document.querySelector(".progress-bar");if(progressElement){new ScrollProgress(function(x,y){progressElement.style.width=y*100+"%"
 })}$(".toc-child").hide();$(".toc-link").on("click",function(e){e.preventDefault();
-toToc($(this).attr("href"))});var expandToc=function expandToc($item){if($item.is(":visible")){return
-}$item.fadeIn(400)};var isanchor=yiliaConfig.anchor;var updateAnchor=function updateAnchor(anchor){if(window.history.replaceState&&anchor!==window.location.hash){window.history.replaceState(undefined,undefined,anchor)
-}};$("#container").scroll(function(){var currentTop=$(this).scrollTop();findPcHeadPosition(currentTop)
-});$(window).scroll(function(){var currentTop=$(this).scrollTop()||document.documentElement.scrollTop;
-findMobileHeadPosition(currentTop+50)});var findPcHeadPosition=function findPcHeadPosition(top){if($(".toc-link").length===0){return false
+toToc($(this).attr("href"))});function toToc(name){var scrollPositionTop=$(name).position().top+50;
+$("#container").animate({scrollTop:scrollPositionTop},1000);var scrollOffset=$(name).offset();
+$("body,html").animate({scrollTop:scrollOffset.top-50})}var isanchor=yiliaConfig.anchor;
+var updateAnchor=function updateAnchor(anchor){if(window.history.replaceState&&anchor!==window.location.hash){window.history.replaceState(undefined,undefined,anchor)
+}};var expandToc=function expandToc($item){if($item.is(":visible")){return}$item.fadeIn(400)
+};var autoScrollToc=function autoScrollToc(){if($(".toc-link").hasClass("active")){var activePosition=$(".active").position().top;
+var articleScrolltop=$(".tooltip-content .toc-article").scrollTop();if(activePosition>$(".tooltip-content .toc-article").height()-50){$(".tooltip-content .toc-article").scrollTop(articleScrolltop+100)
+}if(activePosition<50){$(".tooltip-content .toc-article").scrollTop(articleScrolltop-100)
+}}};$("#container").scroll(function(){var currentTop=$(this).scrollTop();findPcHeadPosition(currentTop);
+autoScrollToc()});var findPcHeadPosition=function findPcHeadPosition(top){if($(".toc-link").length===0){return false
 }var list=$(".article-entry").find("h1,h2,h3,h4,h5,h6");var currentId="";list.each(function(){var head=$(this);
 if(top>head.position().top){currentId="#"+$(this).attr("id")}});if(currentId===""){$(".toc-link").removeClass("active");
 $(".toc-child").hide()}var currentActive=$(".toc-link.active");if(currentId&&currentActive.attr("href")!==currentId){if(isanchor){updateAnchor(currentId)
 }$(".toc-link").removeClass("active");var _this=$('.toc-link[href="'+currentId+'"]');
 _this.addClass("active");var parents=_this.parents(".toc-child");var topLink=parents.length>0?parents.last():_this;
 expandToc(topLink.closest(".toc-item").find(".toc-child"));topLink.closest(".toc-item").siblings(".toc-item").find(".toc-child").hide()
-}};var findMobileHeadPosition=function findMobileHeadPosition(top){if($(".toc-link").length===0){return false
+}};$(window).scroll(function(){var currentTop=$(this).scrollTop()||document.documentElement.scrollTop;
+findMobileHeadPosition(currentTop+50);autoScrollToc()});var findMobileHeadPosition=function findMobileHeadPosition(top){if($(".toc-link").length===0){return false
 }var list=$(".article-entry").find("h1,h2,h3,h4,h5,h6");var currentId="";list.each(function(){var head=$(this);
 if(top>head.offset().top-25){currentId="#"+$(this).attr("id")}});if(currentId===""){$(".toc-link").removeClass("active");
 $(".toc-child").hide()}var currentActive=$(".toc-link.active");if(currentId&&currentActive.attr("href")!==currentId){if(isanchor){updateAnchor(currentId)
 }$(".toc-link").removeClass("active");var _this=$('.toc-link[href="'+currentId+'"]');
 _this.addClass("active");var parents=_this.parents(".toc-child");var topLink=parents.length>0?parents.last():_this;
 expandToc(topLink.closest(".toc-item").find(".toc-child"));topLink.closest(".toc-item").siblings(".toc-item").find(".toc-child").hide()
-}};function toToc(name){var scrollPositionTop=$(name).position().top+50;$("#container").animate({scrollTop:scrollPositionTop},1000);
-var scrollOffset=$(name).offset();$("body,html").animate({scrollTop:scrollOffset.top-50})
-};
+}};
