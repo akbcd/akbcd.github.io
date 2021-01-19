18d3983b702c44fa454a96eb9047414cde16672c
akbcd <2226227487@qq.com>
2021-01-19 20:57:28 +0800

    Site updated: 2021-01-19 20:56:47

--- backup/backup_script.js
+++ backup/backup_script.js
@@ -3,8 +3,8 @@ open_in_new
 新窗口打开链接
 */
 if (yiliaConfig.open_in_new) {
-  for (var open = document.querySelectorAll(".article-title,.article-more-a,.archive-article-date,.archive-article-title"), i = 0; i < open.length; ++i) open[i].setAttribute("target", "_blank");
-  if (yiliaConfig.isPost) for (var close = document.querySelectorAll(".article-title,.archive-article-date"), i = 0; i < close.length; ++i) close[i].removeAttribute("target");
+  document.querySelectorAll(".article-title,.article-more-a,.archive-article-date,.archive-article-title").forEach(i=>{i.setAttribute("target", "_blank")});
+  if (yiliaConfig.isPost) document.querySelectorAll(".article-title,.archive-article-date").forEach(i=>{i.removeAttribute("target")});
 }
 
 /*
@@ -17,159 +17,149 @@ if (progressElement) {
       progressElement.style.width = y * 100 + '%';
   });
 }
-
+var $container=document.getElementById("container");
 /*
 toc目录优化，jq实现锚点跳转
 依赖jquery
 */
-// 隐藏子目录
-$('.toc-child').hide();
-// 目录锚点跳转
-$('.toc-link').on('click', function (e) {
-    e.preventDefault();
-    toToc(decodeURI($(this).attr('href')));
-});
-function toToc (name) {
-  //pc锚点跳转
-  var scrollPositionTop = $(name).position().top+50;
-  $('#container').animate({
-    scrollTop: scrollPositionTop
-  },1000);
-  //mobile锚点跳转
-  var scrollOffset = $(name).offset();
-  $('body,html').animate({
-    scrollTop: scrollOffset.top-50
+if(yiliaConfig.isPost&&typeof(jQuery)!='undefined'){
+  //遍历toc-child
+  function tocChild(key,value){
+    for(var i=0;i<$toc_child.length;i++){
+      $toc_child[i].style.cssText=key+":"+value;
+    }
+  };
+  //获取元素绝对位置
+  function getElementTop(element){
+    var actualTop=element.offsetTop;
+    var current=element.offsetParent;
+    while(current!==null){
+      actualTop+=current.offsetTop;
+      current=current.offsetParent;
+    }
+    return actualTop;
+  };
+  function toToc (name) {
+    //目标元素
+    var $name=document.getElementById(name.substring(1));
+    //目标元素位置
+    var scrollPositionTop = getElementTop($name);
+    $('#container').animate({
+      scrollTop: scrollPositionTop
+    },1000);
+    //mobile锚点跳转
+    var scrollOffset = $(name).offset();
+    $('body,html').animate({
+      scrollTop: scrollOffset.top-50
+    },1000);
+  };
+  //获取元素
+  var $toc_article=document.getElementsByClassName("toc-article")[0];
+  var $toc_child=document.getElementsByClassName("toc-child");
+  var $toc_link=document.getElementsByClassName("toc-link");
+  // 隐藏子目录
+  tocChild("display","none");
+  // 添加目录锚点跳转
+  for(var i in $toc_link){
+    $toc_link[i].onclick=function(e){
+        e.preventDefault();
+        toToc(decodeURI(this.getAttribute('href')));
+    };
+  };
+  // 更新锚点url
+  var isanchor = yiliaConfig.anchor;
+  var updateAnchor = function (anchor) {
+    if (window.history.replaceState && anchor !== window.location.hash) {
+      window.history.replaceState(undefined, undefined, anchor);
+    }
+  };
+  // 显示子目录
+  var expandToc = function ($item) {
+    if ($item.is(':visible')) {
+      return;
+    }
+    $item.fadeIn(400);
+  };
+  // 目录滚动
+  var autoScrollToc = function () {
+    if ($('.toc-link').hasClass('active')) {
+      var activePosition = $('.active').position().top;
+      var articleScrolltop =$toc_article.scrollTop;
+      if (activePosition > $toc_article.clientHeight-50) {
+        $toc_article.scrollTop=articleScrolltop+150;
+      }
+      if (activePosition < 50) {
+        $toc_article.scrollTop=articleScrolltop-150;
+      }
+    }
+  };
+  // pc动态目录
+  $container.addEventListener('scroll', (e) => {
+    var currentTop = $container.scrollTop;
+    findHeadPosition(currentTop,true);
+    autoScrollToc();
   });
-};
-// 更新锚点url
-var isanchor = yiliaConfig.anchor;
-var updateAnchor = function (anchor) {
-  if (window.history.replaceState && anchor !== window.location.hash) {
-    window.history.replaceState(undefined, undefined, anchor);
-  }
-};
-// 显示子目录
-var expandToc = function ($item) {
-  if ($item.is(':visible')) {
-    return;
-  }
-  $item.fadeIn(400);
-};
-// 目录滚动
-var autoScrollToc = function () {
-  if ($('.toc-link').hasClass('active')) {
-    var activePosition = $('.active').position().top;
-    var articleScrolltop = $('.tooltip-content .toc-article').scrollTop();
-    if (activePosition > $('.tooltip-content .toc-article').height()-50) {
-      $('.tooltip-content .toc-article').scrollTop(articleScrolltop + 150);
-    }
-    if (activePosition < 50) {
-      $('.tooltip-content .toc-article').scrollTop(articleScrolltop - 150);
-    }
-  }
-};
-//获取hexo版本号
-var hexoVersion=(null!=document.querySelector('meta[name="generator"]'))?document.querySelector('meta[name="generator"]').getAttribute("content").substring(5,6):null;
-// pc动态目录
-$('#container').scroll(function() {
-  var currentTop = $(this).scrollTop();
-  findPcHeadPosition(currentTop);
-  autoScrollToc();
-});
-// find head position & add active class
-// DOM Hierarchy:
-// ol.toc > (li.toc-item, ...)
-// li.toc-item > (a.toc-link, ol.toc-child > (li.toc-item, ...))
-var findPcHeadPosition = function (top) {
-// assume that we are not in the post page if no TOC link be found,
-// thus no need to update the status
-  if ($('.toc-link').length === 0) {
-    return false;
-  };
-  var list = $('#article-content').find('h1,h2,h3,h4,h5,h6');
-  var currentId = '';
-  list.each(function () {
-    var head = $(this);
-    if (top > head.position().top) {
-      currentId = '#' + $(this).attr('id');
-    }
+  // mobile动态目录
+  window.addEventListener('scroll', (e) => {
+    var currentTop = document.body.scrollTop||document.documentElement.scrollTop;
+    findHeadPosition(currentTop+50,false);
+    autoScrollToc();
   });
-
-  if (currentId === '') {
-    $('.toc-link').removeClass('active');
-    $('.toc-child').hide();
-  };
-
-  var currentActive = $('.toc-link.active');
-  if (currentId && decodeURI(currentActive.attr('href')) !== currentId) {
-    if (isanchor) updateAnchor(currentId);
-
-    $('.toc-link').removeClass('active');
-    var _this=(null!=hexoVersion&&hexoVersion>=5)?$('.toc-link[href="'+encodeURI(currentId)+'"]'):$('.toc-link[href="'+currentId+'"]');
-    _this.addClass('active');
-
-    var parents = _this.parents('.toc-child');
-    // Returned list is in reverse order of the DOM elements
-    // Thus `parents.last()` is the outermost .toc-child container
-    // i.e. list of subsections
-    var topLink = (parents.length > 0) ? parents.last() : _this;
-    expandToc(topLink.closest('.toc-item').find('.toc-child'));
-    topLink
-    // Find all top-level .toc-item containers, i.e. sections
-    // excluding the currently active one
-    .closest('.toc-item').siblings('.toc-item')
-    // Hide their respective list of subsections
-    .find('.toc-child').hide();
-  }
-};
-// mobile动态目录
-$(window).scroll(function() {
-  var currentTop = $(this).scrollTop()||document.documentElement.scrollTop;
-  findMobileHeadPosition(currentTop+50);
-  autoScrollToc();
-});
-var findMobileHeadPosition = function (top) {
-// assume that we are not in the post page if no TOC link be found,
-// thus no need to update the status
-  if ($('.toc-link').length === 0) {
-    return false;
-  };
-  var list = $('#article-content').find('h1,h2,h3,h4,h5,h6');
-  var currentId = '';
-  list.each(function () {
-    var head = $(this);
-    if (top > head.offset().top-25) {
-      currentId = '#' + $(this).attr('id');
+  // find head position & add active class
+  // DOM Hierarchy:
+  // ol.toc > (li.toc-item, ...)
+  // li.toc-item > (a.toc-link, ol.toc-child > (li.toc-item, ...))
+  var findHeadPosition = function (top,isPc) {
+  // assume that we are not in the post page if no TOC link be found,
+  // thus no need to update the status
+    if ($toc_link.length === 0) {
+      return false;
+    };
+    var list = $('#article-content').find('h1,h2,h3,h4,h5,h6');
+    var currentId = '';
+    if(isPc){
+      list.each(function () {
+        var head = $(this);
+        if (top > head.position().top) {
+          currentId = '#' + $(this).attr('id');
+        }
+      });
+    }else{
+      list.each(function () {
+        var head = $(this);
+        if (top > head.offset().top-25) {
+          currentId = '#' + $(this).attr('id');
+        }
+      });
     }
-  });
+    //获取hexo版本号
+    var hexoVersion=(null!=document.querySelector('meta[name="generator"]'))?document.querySelector('meta[name="generator"]').getAttribute("content").substring(5,6):null;
+    var currentActive = $toc_article.querySelectorAll('.active');
+    if (currentId === '') {
+      currentActive.forEach(i => { i.classList.remove('active')});
+      tocChild("display","none");
+    };
+    if (currentId && decodeURI(currentActive[0]!=null?currentActive[0].getAttribute('href'):null) !== currentId) {
+      if (isanchor) updateAnchor(currentId);
 
-  if (currentId === '') {
-    $('.toc-link').removeClass('active');
-    $('.toc-child').hide();
+      $toc_article.querySelectorAll('.active').forEach(i => { i.classList.remove('active')});
+      var _this=(null!=hexoVersion&&hexoVersion>=5)?$('.toc-link[href="'+encodeURI(currentId)+'"]'):$('.toc-link[href="'+currentId+'"]');
+      _this.addClass('active');
+
+      var parents = _this.parents('.toc-child');
+      // Returned list is in reverse order of the DOM elements
+      // Thus `parents.last()` is the outermost .toc-child container
+      // i.e. list of subsections
+      var topLink = (parents.length > 0) ? parents.last() : _this;
+      expandToc(topLink.closest('.toc-item').find('.toc-child'));
+      topLink
+      // Find all top-level .toc-item containers, i.e. sections
+      // excluding the currently active one
+      .closest('.toc-item').siblings('.toc-item')
+      // Hide their respective list of subsections
+      .find('.toc-child').hide();
+    }
   };
-
-  var currentActive = $('.toc-link.active');
-  if (currentId && decodeURI(currentActive.attr('href')) !== currentId) {
-    if (isanchor) updateAnchor(currentId);
-
-    $('.toc-link').removeClass('active');
-
-    var _this=(null!=hexoVersion&&hexoVersion>=5)?$('.toc-link[href="'+encodeURI(currentId)+'"]'):$('.toc-link[href="'+currentId+'"]');
-    _this.addClass('active');
-
-    var parents = _this.parents('.toc-child');
-    // Returned list is in reverse order of the DOM elements
-    // Thus `parents.last()` is the outermost .toc-child container
-    // i.e. list of subsections
-    var topLink = (parents.length > 0) ? parents.last() : _this;
-    expandToc(topLink.closest('.toc-item').find('.toc-child'));
-    topLink
-    // Find all top-level .toc-item containers, i.e. sections
-    // excluding the currently active one
-    .closest('.toc-item').siblings('.toc-item')
-    // Hide their respective list of subsections
-    .find('.toc-child').hide();
-  }
 };
 
 /*
@@ -178,20 +168,20 @@ var findMobileHeadPosition = function (top) {
 if(yiliaConfig.isPost&&yiliaConfig.scrollPos){
   //在即将离开当前页面（刷新或关闭）时触发
   window.onbeforeunload = function(){
-    var scrollPos=document.getElementById('container').scrollTop || document.documentElement.scrollTop || document.body.scrollTop;
+    var scrollPos=$container.scrollTop || document.documentElement.scrollTop || document.body.scrollTop;
     document.cookie="scrollTop="+scrollPos; //存储滚动条位置到cookies中
   };
   //在离在开网页时（点击链接，刷新页面，关闭浏览器等）触发
   window.onpagehide = function(){
-    var scrollPos=document.getElementById('container').scrollTop || document.documentElement.scrollTop || document.body.scrollTop;
+    var scrollPos=$container.scrollTop || document.documentElement.scrollTop || document.body.scrollTop;
     document.cookie="scrollTop="+scrollPos; //存储滚动条位置到cookies中
   };
   window.addEventListener("load",function(){ 
     if(document.cookie.match(/scrollTop=([^;]+)(;|$)/)!=null){
         var arr=document.cookie.match(/scrollTop=([^;]+)(;|$)/); //cookies中不为空，则读取滚动条位置
-        document.getElementById('container').scrollTop=arr[1];
+        $container.scrollTop=arr[1];
         document.documentElement.scrollTop=parseInt(arr[1]);
         document.body.scrollTop=parseInt(arr[1]);
     }
   });
-}
+};
